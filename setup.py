4569f2117b441475fdd9b01d7cef45831d8c409c
Mathias Ertl <mati@fsinf.at>
2013-12-21 11:15:32 +0100

    add setup/distribute_setup files

---
+++ setup.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-hashers-passlib
+# (https://github.com/mathiasertl/django-hashers-passlib).
+#
+# django-hashers-passlib is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# django-hashers-passlib is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# django-hashers-passlib.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import unittest
+
+try:
+    from setuptools import Command
+    from setuptools import setup
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import Command
+    from setuptools import setup
+
+from distutils.command.clean import clean as _clean
+
+from test import test_hashers
+
+name = 'django-hashers-passlib'
+url = 'https://github.com/mathiasertl/django-hashers-passlib'
+version = '0.1'
+requires = ['passlib>=1.6', 'Django>=1.6' ]
+
+
+class clean(_clean):
+    pass
+
+
+class test(Command):
+    description = 'Run test suite.'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        loader = unittest.TestLoader()
+        suite = loader.loadTestsFromModule(test_hashers)
+        unittest.TextTestRunner(verbosity=1).run(suite)
+
+setup(
+    name=name,
+    version=version,
+    description='Django hashers using passlib',
+    long_description="""TODO.""",
+    author='Mathias Ertl',
+    author_email='mati@er.tl',
+    url=url,
+#    download_url='https://python.restauth.net/download/',
+    packages=['hashers_passlib', ],
+    cmdclass={
+        'clean': clean,
+        'test': test,
+    },
+    license="GNU General Public License (GPL) v3",
+    install_requires=requires,
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Other Environment",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration :: Authentication/Directory",
+    ]
+)
