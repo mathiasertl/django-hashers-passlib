247a913dc4b8d10eda6cfcd32515940cfe582bbc
Mathias Ertl <mati@fsinf.at>
2014-01-01 14:21:40 +0100

    add long description

--- setup.py
+++ setup.py
@@ -69,7 +69,13 @@ setup(
     name=name,
     version=version,
     description='Django hashers using passlib',
-    long_description="""TODO.""",
+    long_description="""This library provides password hashers for the hash
+schemes provided by passlib for Djangos password hashing framework. Unlike
+passlibs ``passlib.apps.django``, it does not replace Djangos password hashing
+framework but adds additional hashers to its built in framework.
+
+Please see https://github.com/mathiasertl/django-hashers-passlib for more
+information and documentation.""",
     author='Mathias Ertl',
     author_email='mati@er.tl',
     url=url,
