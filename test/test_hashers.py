0016cb53758d5720ebcfcca62f50e149985053ea
Mathias Ertl <mati@er.tl>
2015-12-05 15:02:04 +0100

    remove unused import

--- test/test_hashers.py
+++ test/test_hashers.py
@@ -28,7 +28,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'example.settings'
 
 from passlib import hash
 
-from django.conf import settings
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.hashers import make_password
 from django.test import TestCase
